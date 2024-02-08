949ef4f63ed8ea4694257d2f96d378997503f168
roddy12 <roddydeveloper2023@gmail.com>
2024-02-07 21:33:56 -0600

    config

--- Portafolio-Seo/astro.config.mjs
+++ Portafolio-Seo/astro.config.mjs
@@ -5,6 +5,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: vercel(),
+  site: 'https://roddyvargas.github.io/Portafolio-Seo/',
+  base: '/Portafolio-seo/',
   integrations: [tailwind()]
 });
